7f3dfef833089ec62f9f7387624302f4e88af397
Carlosgrm1444 <117303471+Carlosgrm1444@users.noreply.github.com>
2023-05-24 21:52:39 -0500

    xd

---
+++ 01-dart-Intro/08_getters_setters.dart
@@ -0,0 +1,32 @@
+void main() {
+  final mySquare = Square(side: 10);
+
+  mySquare.side = -5;
+
+  print('Area: ${mySquare.calcularArea()}');
+
+}
+
+class Square {
+  double _side;
+
+  Square({required double side})
+  
+  : assert (side >=0, 'el lado debe de ser mayor a 0'),
+  _side = side;
+
+  double get area{
+    return _side * _side;
+  }
+
+  set side(double value){
+print('Sentting new value $value');
+  if  (value < 0 ) throw 'El valor tiene que se >=';
+
+  _side = value;
+  }
+
+  double calcularArea(){
+return _side * _side;
+  }
+}
---
+++ 01-dart-Intro/09_abstract_class.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  
+}
+
+enum PlantType{nuclear,wind,water}
+
+abstract class EnergyPlant{
+
+  double energyLeft;
+  PlantType type;
+
+  EnergyPlant({required this.energyLeft, required this.type});
+
+  void consumeEnergy(double amount);
+}
