bfc3e58479e3d50b1e0d477c100221636e7241bd
Carlosgrm1444 <117303471+Carlosgrm1444@users.noreply.github.com>
2023-05-25 23:29:08 -0500

    leccion 9

--- 01-dart-Intro/08_getters_setters.dart
+++ 01-dart-Intro/08_getters_setters.dart
@@ -4,29 +4,27 @@ void main() {
   mySquare.side = -5;
 
   print('Area: ${mySquare.calcularArea()}');
-
 }
 
 class Square {
   double _side;
 
   Square({required double side})
-  
-  : assert (side >=0, 'el lado debe de ser mayor a 0'),
-  _side = side;
+      : assert(side >= 0, 'el lado debe de ser mayor a 0'),
+        _side = side;
 
-  double get area{
+  double get area {
     return _side * _side;
   }
 
-  set side(double value){
-print('Sentting new value $value');
-  if  (value < 0 ) throw 'El valor tiene que se >=';
+  set side(double value) {
+    print('Sentting new value $value');
+    if (value < 0) throw 'El valor tiene que se >=';
 
-  _side = value;
+    _side = value;
   }
 
-  double calcularArea(){
-return _side * _side;
+  double calcularArea() {
+    return _side * _side;
   }
 }
